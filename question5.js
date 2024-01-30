9bd6459842242e97cbcbc569b993d45fcae09134
UdayBohra7 <143582686+UdayBohra7@users.noreply.github.com>
2024-01-30 15:32:42 +0530

    final commit

--- question5.js
+++ question5.js
@@ -3,29 +3,14 @@ let, var, and const are all keywords used for variable declaration in JavaScript
 but they have some differences:
 */
 
-// Example 1: 
-// block scope declaration
-{
-    var varVal = 8;
-    const constVal = 5;
-    let letVal = 7;
-    letVal = 5;
-    console.log(`const: ${constVal}`);   // O/P = 5
-}
-console.log(`var: ${varVal}`); // O/P = 8
-// console.log(`let: ${letVal}`); // throw an error
-// console.log(`const: ${constVal}`); // throw an error
-
-
-// Example 2:
 // Example with var
 function varExample() {
     var x = 10;
     if (true) {
         var x = 20; // This redeclaration affects the outer scope
-        console.log(x); // Output: 20
+        console.log("var: ", x); // Output: 20
     }
-    console.log(x); // Output: 20
+    console.log("outer var: ", x); // Output: 20
 }
 varExample();
 
@@ -34,9 +19,9 @@ function letExample() {
     let y = 10;
     if (true) {
         let y = 20; // This is a new variable declaration in a different block scope
-        console.log(y); // Output: 20
+        console.log("let: ", y); // Output: 20
     }
-    console.log(y); // Output: 10 (the outer 'y' is not affected)
+    console.log("outer let: ", y); // Output: 10 (the outer 'y' is not affected)
 }
 letExample();
 
@@ -45,13 +30,14 @@ function constExample() {
     const z = 10;
     if (true) {
         // const z = 20; // This would throw an error: SyntaxError: Identifier 'z' has already been declared
-        console.log(z); // Output: 10
+        console.log("const: ", z); // Output: 10
     }
-    console.log(z); // Output: 10
+    console.log("outer const: ", z); // Output: 10
 }
 constExample();
 
 // Reassignment with let and const
+console.log("reassignment with let and const: ")
 let a = 10;
 a = 20; // This is allowed with let
 console.log(a); // Output: 20
